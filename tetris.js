7ed4a59b69eb5c3ada90f3fc89b9ae9fec9ca0cb
rinw <rnu@usa.com>
2016-10-10 02:30:12 +0300

    marching on to success

--- tetris.js
+++ tetris.js
@@ -6,19 +6,27 @@
 // 		rotation: 0, 90, 180, 270
 //		color: "#hhhhhh", false
 
+// **** Game State **** 
+
+var board_size = {'width': 25, 'height': 40}
+var board
+var p 
+var state
 
 // **** Game Mechanics **** 
 
-
-function create_board (size) {
-   var board = []
-	for (var y = 0; y < size.height; y++) {		
+function create_board () {
+	board = []
+	for (var y = 0; y < board_size.height; y++) {		
 		board[y] = []
-		for (var x = 0; x < size.width; x++) {
+		for (var x = 0; x < board_size.width; x++) {
 			board[y][x] = false
 		}
 	}
-	return board
+}
+
+function create_piece(type, x, y, r, color) {
+	p = {'x': x, 'y': y, 'r': r, 'type': type, 'color': color }
 }
 
 var masks = [
@@ -37,7 +45,7 @@ var masks = [
 	 'xxx']
 ]
 
-function rotated_mask(mask, r) {
+function rotated_mask (mask, r) {
 	r = r % 360
 	if (!r) 
 		return mask
@@ -61,38 +69,41 @@ function rotated_mask(mask, r) {
 	assert(false)
 }
 
-function get_mask(p, r) {
+function piece_mask (r) {
 	if (r === undefined)
 		r = p.r
 	var mask = masks[p.type]
 	return rotated_mask(mask, r)
 }
 
-function paint (board, p, color) {
-	if (color === undefined) {
-		color = p.color
-	}
-	var mask = get_mask(p)	
-	for (var my = 0; my < mask.length; my++) {
-		for (var mx = 0; mx < mask[my].length; mx++) {
-			if (mask[my][mx] == 'x') {
-				var x = p.x + mx
-				var y = p.y + my
-				board[y][x] = color 
-			}
-		}
-	}
-}
-
-function piece_size (p, r) {
-	var mask = get_mask(p, r)
+function piece_size (r) {
+	var mask = piece_mask(r)
 	var w = mask[0].length
 	var h = mask.length
 	return {'width': w, 'height': h }
 }
 
-function inside_board (board, p, x, y, r) {
-	var size = piece_size(p, r)
+var change_square_color // forward declaration
+
+function paint_piece (color) {
+	if (color === undefined) 
+		color = p.color
+	var mask = piece_mask()
+	var size = piece_size()
+	for (var my = 0; my < size.height; my++) {
+		for (var mx = 0; mx < size.width; mx++) {
+			if (mask[my][mx] == 'x') {
+				var x = p.x + mx
+				var y = p.y + my
+				board[y][x] = color 
+				change_square_color(x, y, color)
+			}
+		}
+	}
+}
+
+function piece_inside_board (x, y, r) {
+	var size = piece_size(r)
 	var w = size.width
 	var h = size.height
 	var bw = board[0].length
@@ -100,64 +111,63 @@ function inside_board (board, p, x, y, r) {
 	return x >= 0 && y >= 0 && x <= bw - w && y <= bh - h
 }
 
-function update (board, p, x, y, r) {
-	if(!inside_board(board, p, x, y, r))
+function can_move (x, y, r) {
+	if(!piece_inside_board(x, y, r))
+		return false
+	// TODO: check if any of the piece's squares overlaps with board squares
+	return true
+}
+
+function update_piece (x, y, r) {
+	if(!can_move(x, y, r))
 		return
-	paint(board, p, false) // erase the piece on current position
+	paint_piece(false) // erase the piece on current position
 	p.x = x 
 	p.y = y
 	p.r = r
-	paint(board, p)
+	paint_piece()
 }
-
-//returns true if the piece is on deck, false if it isn't
-
-
-function on_deck (board, p) {
-
-}
-
-var can_move = function (p, x, y, r) {}
-
-var erase_full_lines = function () {}
-
-// **** Game Logic ****
-
-var board
-var p 
-var state
-
-function init_piece (size){
-	var type = Math.floor(Math.random() * masks.length)
-	p = {'x': 0, 'y': 0, 'r': 0, 'type': type, 'color': '#ff0000' }	
-	p.x = Math.floor((size.width - piece_size(p).width)/2)
-	paint(board, p)
-}
-
-function restart_game (size) {
-	state = 'running'
-	board = create_board(size)
-	init_piece(size)
-}
-
-
 
 // how : 'left', 'right', 'down', 'rotate'
-function move (how) {
+function move_piece (how) {
 	if (how == 'left') {
-		update(board, p, p.x-1, p.y, p.r)
+		update_piece(p.x-1, p.y, p.r)
 	}
 	else if (how == 'right') {
-		update(board, p, p.x+1, p.y, p.r)		
+		update_piece(p.x+1, p.y, p.r)		
 	}
 	else if (how == 'down') {
-		update(board, p, p.x, p.y+1, p.r)		
+		update_piece(p.x, p.y+1, p.r)		
 	}
 	else if (how == 'rotate') {
-		update(board, p, p.x, p.y, p.r+90)		
+		update_piece(p.x, p.y, p.r+90)		
 	}
 }
 
+// returns true if the piece is on deck, false if it isn't
+function on_deck () {
+	var mask = piece_mask()
+	var size = piece_size()	
+}
+
+function erase_full_lines () {}
+
+var colors = ['#ff0000', '#0000ff', '#00ff00', '#ffff00']
+
+function init_piece() {
+	var type = Math.floor(Math.random() * masks.length)
+	var color = colors[Math.floor(Math.random() * colors.length)]
+	create_piece(type, 0, 0, 0, color)
+	// center the piece horizontally to the board
+	p.x = Math.floor((board_size.width - piece_size().width)/2) 
+}
+
+function restart_game () {
+	create_board()
+	init_piece()
+	paint_piece()
+	state = 'running'
+}
 
 // returns square color, or false for no color
 function get_square (x, y) {
@@ -170,15 +180,13 @@ function get_state () {
 	return state
 }
 
-
 // **** Game Interface ****
-var size = {'width': 25, 'height': 40}
 
-function draw_board(size) {
+function draw_board() {
 	var s = ''
-	for (var y = 0; y < size.height; y++) {
+	for (var y = 0; y < board_size.height; y++) {
 		s = s + '<div class=row>'
-		for (var x = 0; x < size.width; x++) {
+		for (var x = 0; x < board_size.width; x++) {
 			var color = get_square(x, y)
 			if (color == false)
 				color = '#000000'
@@ -190,24 +198,33 @@ function draw_board(size) {
 
 }
 
+function move_piece_and_draw_board (how) {
+	move_piece(how)
+	draw_board()
+}
+
+function handle_keydown (e) {
+    if (e.keyCode == 37)  // left arrow
+   		move_piece_and_draw_board('left') 
+    else if (e.keyCode == 39) // right arrow
+    	move_piece_and_draw_board('right')
+    else if (e.keyCode == 40) // down arrow 
+    	move_piece_and_draw_board('down')
+    else if (e.keyCode == 38) // space key
+		move_piece_and_draw_board('rotate')
+}
+
+change_square_color = function (x, y, color) { // declared above
+	// TODO
+}
 
 function main () {
-	restart_game(size)
-	draw_board(size)
-	$(document).keydown(function(e) {
-		var show_move = function (how) {
-			move(how)
-			draw_board(size)
-		}
-        if (e.keyCode == 37)  // left arrow
-       		show_move('left') 
-        else if (e.keyCode == 39) // right arrow
-        	show_move('right')
-        else if (e.keyCode == 40) // down arrow 
-        	show_move('down')
-        else if (e.keyCode == 38) // space key
- 			show_move('rotate')
-    })
+	restart_game()
+	draw_board()
+	$(document).keydown(handle_keydown)
+	setInterval(function() { 
+		move_piece_and_draw_board('down') 
+	}, 1000)
 }
 
 $(main)
